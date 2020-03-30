bf463b52c7ac160988d351802ba3b6e63ff1cc5e
Carlos <Tapiacarlos32@gmail.com>
2020-03-30 13:30:45 -0500

    Template inheritance, migrations, admin panel

--- DjangoCylusProject/DjangoCylusProject/urls.py
+++ DjangoCylusProject/DjangoCylusProject/urls.py
@@ -17,16 +17,17 @@ Including another URLconf
 """
 
 # Uncomment next two lines to enable admin:
-#from django.contrib import admin
-#from django.urls import path
+from django.contrib import admin
+from django.urls import path
 
 from django.conf.urls import url,include
 from Cylus import views
 
 urlpatterns = [
     # Uncomment the next line to enable the admin:
-    #url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     url(r'^$',views.home, name = 'home'),
+    url(r'^home/$',views.home, name = 'home'),
     url(r'^about/$',views.AboutView.as_view(),name = 'about'),
     url(r'^contact/$',views.ContactView.as_view(), name='contact')
 ]
